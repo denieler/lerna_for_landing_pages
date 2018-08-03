e57270f70553e99fa269ec6a0b390c0e8813a1d6
Daniel Ostapenko <danielostapenko@gmail.com>
2018-08-03 11:16:29 +0200

    Initial commit

---
+++ packages/project1/__tests__/project1.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('project1', () => {
+    it('needs tests', () => {
+        expect(1).toBe(1)
+    });
+});
---
+++ packages/project2/__tests__/project2.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('project2', () => {
+    it('needs tests', () => {
+        expect(1).toBe(1)
+    });
+});
